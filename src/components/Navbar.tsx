9b1e719cda5dbf5335810fb876148b07a083783d
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-02-19 19:38:19 +0530

    Logo Degined

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar = () => {
       initial={{ y: -80 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.6 }}
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300  ${
         scrolled ? "bg-glass border-b border-border" : ""
       }`}
     >
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         <a href="#home" className="text-xl font-bold text-gradient-pink">
-          {"<Dev />"}
+          {"<Code/Well>"}
         </a>
 
         {/* Desktop */}
